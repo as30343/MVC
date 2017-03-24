a5cd4e9cce8ecd86fa2f28760986b2a0a895c148
as30343 <1103137136@gm.kuas.edu.tw>
2017-03-24 15:05:49 +0800

    new comment (order)

--- System/Areas/Order/Controllers/OrderController.cs
+++ System/Areas/Order/Controllers/OrderController.cs
@@ -10,26 +10,50 @@ namespace System.Areas.Order.Controllers
     {
         //
         // GET: /Order/Order/
+        /// <summary>
+        /// 訂單管理系統_首頁
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View();
         }
+        /// <summary>
+        /// 訂單管理系統_查詢
+        /// </summary>
+        /// <returns></returns>
         public ActionResult ShowResult()
         {
             return View();
         }
+        /// <summary>
+        /// 訂單管理系統_查詢細項
+        /// </summary>
+        /// <returns></returns>
         public ActionResult ShowDetail()
         {
             return View();
         }
+        /// <summary>
+        /// 訂單管理系統_新增
+        /// </summary>
+        /// <returns></returns>
         public ActionResult InsertOrder()
         {
             return View();
         }
+        /// <summary>
+        /// 訂單管理系統_修改
+        /// </summary>
+        /// <returns></returns>
         public ActionResult UpdateOrder()
         {
             return View();
         }
+        /// <summary>
+        /// 訂單管理系統_刪除
+        /// </summary>
+        /// <returns></returns>
         public ActionResult DeleteOrder()
         {
             return View();
