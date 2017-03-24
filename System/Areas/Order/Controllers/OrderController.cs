9d0482719bca8e7dfa2ea9f01910ccf62dd99a23
as30343 <1103137136@gm.kuas.edu.tw>
2017-03-24 14:53:36 +0800

    new controller new action (Order)

---
+++ System/Areas/Order/Controllers/OrderController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace System.Areas.Order.Controllers
+{
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Order/Order/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ShowResult()
+        {
+            return View();
+        }
+        public ActionResult ShowDetail()
+        {
+            return View();
+        }
+        public ActionResult InsertOrder()
+        {
+            return View();
+        }
+        public ActionResult UpdateOrder()
+        {
+            return View();
+        }
+        public ActionResult DeleteOrder()
+        {
+            return View();
+        }
+	}
+}
